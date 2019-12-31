4b37115df629fc8f2775d6438791045d142d0934
James Shipman <jbshipman@icloud.com>
2019-12-31 16:00:37 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -11,7 +11,7 @@ def add_character(show, name)
   
   BASE_HOA[:chipmunks] << "Dave"
   BASE_HOA[:third_earthers] << "Snarf"
-  BASE_HOA[:jetsons] << "Snarf"
+  BASE_HOA[:jetsons] << "Astro"
   return BASE_HOA
   
 end
